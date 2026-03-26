b314c75e643022a8a2a229f9725d61e33e76d282
Asmothkhan <asmotkhan793@gmail.com>
2026-03-27 00:45:49 +0600

    solve

--- src/Component/RatingBarChart/RatingBarChart.jsx
+++ src/Component/RatingBarChart/RatingBarChart.jsx
@@ -2,42 +2,70 @@ import React from 'react';
 import { Bar, BarChart, XAxis, YAxis } from 'recharts';
 import { Tooltip, CartesianGrid } from 'recharts';
 
+
+
 const RatingBarChart = ({ app }) => {
-    // const { ratings } = app;
-   const ratings = Array.isArray(app?.ratings) ? app.ratings : [];
-   console.log("ratings:", app?.ratings);
+  const ratings = Array.isArray(app?.ratings) ? app.ratings : [];
+
+  const chartData = ratings.map(rate => ({
+    name: rate.name,
+    count: rate.count
+  }));
+
+  return (
+    <div>
+      <BarChart layout="vertical" width={500} height={300} data={chartData}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis type="number" />
+        <YAxis dataKey="name" type="category" />
+        <Tooltip />
+
+        <Bar 
+          dataKey="count" 
+          fill="yellow" 
+          barSize={20} 
+        />
+      </BarChart>
+    </div>
+  );
+};
+
+// const RatingBarChart = ({ app }) => {
+//     // const { ratings } = app;
+//    const ratings = Array.isArray(app?.ratings) ? app.ratings : [];
+//    console.log("ratings:", app?.ratings);
     
-//      const rating = ratings?.map(rate => ({
+// //      const rating = ratings?.map(rate => ({
+// //   name: rate.name,
+// //   count: rate.count
+// // }));
+
+// const chartData = ratings.map(rate => ({
 //   name: rate.name,
 //   count: rate.count
 // }));
-
-const chartData = ratings.map(rate => ({
-  name: rate.name,
-  count: rate.count
-}));
  
     
     
 
-    return (
-        <div>
+//     return (
+//         <div>
           
-          <BarChart layout="vertical" width={500} height={300} data={chartData}>
-  <CartesianGrid strokeDasharray="3 3" />
-  <XAxis type="number" />
-  <YAxis dataKey="name" type="category" />
-  <Tooltip />
+//           <BarChart layout="vertical" width={500} height={300} data={chartData}>
+//   <CartesianGrid strokeDasharray="3 3" />
+//   <XAxis type="number" />
+//   <YAxis dataKey="name" type="category" />
+//   <Tooltip />
   
-  <Bar 
-    dataKey="count" 
-    fill="yellow" 
-    barSize={20} 
-    // radius={[0, 10, 10, 0]} 
-  />
-</BarChart>
-        </div>
-    );
-};
+//   <Bar 
+//     dataKey="count" 
+//     fill="yellow" 
+//     barSize={20} 
+//     // radius={[0, 10, 10, 0]} 
+//   />
+// </BarChart>
+//         </div>
+//     );
+// };
 
 export default RatingBarChart;
